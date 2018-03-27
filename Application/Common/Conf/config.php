091db9f6390566c9cb676cf666d2c5642bc3ad24
pfq1990 <32444483+pfq1990@users.noreply.github.com>
2018-03-27 23:29:03 +0800

    修改数据库配置文件
    2018-03-27 修改配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,7 +6,7 @@ return array(
     //数据库配置信息
     'DB_TYPE'                       => 'mysql', // 数据库类型
     'DB_HOST'                       => 'localhost', // 服务器地址
-    'DB_NAME'                       => 'auth', // 数据库名
+    'DB_NAME'                       => 'crss', // 数据库名
     'DB_USER'                       => 'root', // 用户名
     'DB_PWD'                        => '123456', // 密码
     'DB_PORT'                       =>  3306, // 端口
